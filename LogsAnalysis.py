7bfb844d1ef0031601c26126bec06e6c9beed2ad
KohlMeister <kohl.meister@3esi-enersight.com>
2018-01-05 16:43:38 -0700

    Fix SQL Convention - Keywords

--- LogsAnalysis.py
+++ LogsAnalysis.py
@@ -15,7 +15,7 @@ def dbquery(query):
 
 # Queries and prints results 1
 def answer_one():
-    query1 = "select title, popular from popular_articles limit 3;"
+    query1 = "SELECT title, popular FROM popular_articles LIMIT 3;"
     results1 = dbquery(query1)
     print '\nTop Three Articles: \n'
     for r in results1:
@@ -24,7 +24,7 @@ def answer_one():
 
 # Queries and prints results 2
 def answer_two():
-    query2 = "select name, pop_auth from popular_authors;"
+    query2 = "SELECT name, pop_auth FROM popular_authors;"
     results2 = dbquery(query2)
     print '\nMost Popular Authors of all Time: \n'
     for r in results2:
@@ -33,7 +33,7 @@ def answer_two():
 
 # Queries and prints results 3
 def answer_three():
-    query3 = "select day, percent from v_errors_answer"
+    query3 = "SELECT day, percent FROM v_errors_answer"
     results3 = dbquery(query3)
     print '\nDays with Errors Greater than One Percent: \n'
     for r in results3:
@@ -47,19 +47,19 @@ def write_to_file():
     f = open("Answers.txt", "w+")
     # Answers to question 1
     f.write("Top Three Arcticles:\n")
-    query1 = "select title, popular from popular_articles limit 3;"
+    query1 = "SELECT title, popular FROM popular_articles LIMIT 3;"
     results1 = dbquery(query1)
     for r in results1:
         f.write('\n"' + str(r[0]) + '" --- ' + str(r[1]) + ' views.')
     # Answers to question 2
     f.write("\n\nMost Popular Authors of all Time:\n")
-    query2 = "select name, pop_auth from popular_authors;"
+    query2 = "SELECT name, pop_auth FROM popular_authors;"
     results2 = dbquery(query2)
     for r in results2:
         f.write('\n' + str(r[0]) + ' --- ' + str(r[1]) + ' views.')
     # Answers to question 3
     f.write("\n\nDays with Errors Greater than One Percent:\n")
-    query3 = "select day, percent from v_errors_answer"
+    query3 = "SELECT day, percent FROM v_errors_answer"
     results3 = dbquery(query3)
     for r in results3:
         f.write('\n' + str(r[0].strftime('%B %d, %Y')) + ' --- ' +
